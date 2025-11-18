917bd4c602606e5569dbcb471a8546854e5e71cf
Estevam Carnecine Silva <estvmc@gmail.com>
2025-11-18 15:27:48 -0300

    feat: Inicializar projeto Spring Boot com Web, JPA, H2 e Security

---
+++ src/main/java/com/consultoriatreinamentodaigo/treinamento_daigo_backend/TreinamentoDaigoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.consultoriatreinamentodaigo.treinamento_daigo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoDaigoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoDaigoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consultoriatreinamentodaigo/treinamento_daigo_backend/TreinamentoDaigoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultoriatreinamentodaigo.treinamento_daigo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoDaigoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
